a810768b2a1ccb55297d1bcd7f5d44f173e9e42a
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-09 21:18:14 -0400

    Update parser.cpp

--- Source/parser.cpp
+++ Source/parser.cpp
@@ -1,9 +1,42 @@
 #include <string>
+#include <vector>
 
 class Command {
-  
+  public:
+    std::string action;
+    std::string[] args;
 }
 
 Command parseCommand(std::string command) {
-  
+  std::string action = "";
+  int stage = 0;
+  int arg = 0;
+  std::vector<std::string> args;
+  int inString = 0;
+  for (int i = 0; i < command.length(); i++) {
+    if (command[i] == ':') {
+      stage = 1;
+      args[0] = "":
+      continue;
+    }
+    if (stage == 0) {
+      action.append(command[i]);
+    } else if (stage == 1) {
+      if (command[i] == '"') {
+        if (inString == 0) {
+          inString = 1;
+        } else {
+          inString = 0;
+        }
+        continue;
+      } else if (command[i] == ',' && inString == 0) {
+        arg++;
+        args[arg] = "";
+      } else if (inString == 1) {
+        args[arg].append(command[i]);
+      }
+    }
+  }
+  Command obj();
+  obj.action = action;
 }
