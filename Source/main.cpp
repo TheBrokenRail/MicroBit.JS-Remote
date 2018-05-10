8bc4de42b3de6a01532c1283e6ac5055973c2605
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-09 20:48:49 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,117 +1,38 @@
 #include <vector>
 #include <cstring>
 #include "MicroBit.h"
-#include "mjs.h"
-#include "JSSource.h"
+#include "MicroBitUARTService.h"
 
 MicroBit uBit;
+MicroBitUARTService *uart;
 
-void uBitDisplayScroll(char *x) {
-  uBit.display.scroll(x);
-}
+int connected = 0;
+int bluetooth = 0;
 
-void uBitDisplayPrint(char *x) {
-  uBit.display.print(x);
-}
-
-void uBitDisplayClear() {
-  uBit.display.clear();
-}
-
-void uBitDisplayImageSetPixelValue(int x, int y, int alpha) {
-  uBit.display.image.setPixelValue(x, y, alpha);
-}
-
-void uBitSleep(int x) {
-  uBit.sleep(x);
-}
-
-void uBitSerialPrintf(char *x) {
-  uBit.serial.printf(x);
-}
-
-char *uBitSerialRead(int x) {
-  return (char *)uBit.serial.read(x).toCharArray();
-}
-
-class uBitListener {
-  public:
-    uBitListener(int sourceObj, int valueObj, void (*callbackObj)(void *), void *userDataObj) {
-      source = sourceObj;
-      value = valueObj;
-      callback = callbackObj;
-      userData = userDataObj;
-    };
-    int source;
-    int value;
-    void (*callback)(void *);
-    void *userData;
-};
-
-std::vector<uBitListener> listeners;
-
-void callListener(MicroBitEvent event) {
-  for (unsigned int i = 0; i < listeners.size(); i++) {
-    if (event.source == listeners[i].source && event.value == listeners[i].value) {
-      listeners[i].callback(listeners[i].userData);
-    }
+void onConnected(MicroBitEvent) {
+  bluetooth = 1;
+  connected = 1;
+  // mobile app will send ASCII strings terminated with the colon character
+  ManagedString eom(":");
+  while(connected == 1) {
+    ManagedString msg = uart->readUntil(eom);
+    uBit.display.scroll(msg);
   }
 }
 
-void uBitMessageBusListen(int source, int value, void (*callback)(void *), void *userData) {
-  uBitListener listener = uBitListener(source, value, callback, userData);
-  listeners.push_back(listener);
-  uBit.messageBus.listen(source, value, callListener);
+void onDisconnected(MicroBitEvent) {
+  bluetooth = 0;
+  connected = 0;
 }
-
-void *ffiResolver(void *handle, const char *name) {
-  if (strcmp(name, "displayScroll") == 0) {
-    return (void *)uBitDisplayScroll;
-  }
-  if (strcmp(name, "displayPrint") == 0) {
-    return (void *)uBitDisplayPrint;
-  }
-  if (strcmp(name, "displayClear") == 0) {
-    return (void *)uBitDisplayClear;
-  }
-  if (strcmp(name, "displayImageSetPixelValue") == 0) {
-    return (void *)uBitDisplayImageSetPixelValue;
-  }
-  if (strcmp(name, "sleep") == 0) {
-    return (void *)uBitSleep;
-  }  if (strcmp(name, "serialPrintf") == 0) {
-    return (void *)uBitSerialPrintf;
-  }
-  if (strcmp(name, "serialRead") == 0) {
-    return (void *)uBitSerialRead;
-  }
-  if (strcmp(name, "messageBusListen") == 0) {
-    return (void *)uBitMessageBusListen;
-  }
-  return NULL;
-}
-
-mjs *mjsObj;
 
 int main() {
   // Initialise the micro:bit runtime.
   uBit.init();
   uBit.display.setDisplayMode(DISPLAY_MODE_GREYSCALE);
 
-  uBit.serial.printf("Create, ");
-  mjsObj = mjs_create();
-  uBit.serial.printf("FFI, ");
-  mjs_set_ffi_resolver(mjsObj, ffiResolver);
-  uBit.serial.printf("JS, ");
-  uBit.serial.printf(jsSource);
-  uBit.serial.printf("Execute, ");
-
-  mjs_err_t err = mjs_exec(mjsObj, jsSource, NULL);
-  if (err) {
-    const char *errStr = mjs_strerror(mjsObj, err);
-    uBit.serial.printf(errStr);
-  }
-  uBit.serial.printf("Done");
+  uBit.messageBus.listen(MICROBIT_ID_BLE, MICROBIT_BLE_EVT_CONNECTED, onConnected);
+  uBit.messageBus.listen(MICROBIT_ID_BLE, MICROBIT_BLE_EVT_DISCONNECTED, onDisconnected);
+  uart = new MicroBitUARTService(*uBit.ble, 32, 32);
 
   // If main exits, there may still be other fibers running or registered event handlers etc.
   // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
