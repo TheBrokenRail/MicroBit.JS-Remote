9866bc67f19525dfa1a4c16bb06a43574f9e6b6d
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-11 16:24:27 -0400

    Update util.cpp

--- Source/util.cpp
+++ Source/util.cpp
@@ -1,6 +1,6 @@
 #include "MicroBit.h"
 #include "parser.h"
 
-void runCommand(Command command, MicroBit uBit) {
+void runCommand(Command command, MicroBit uBit, void (*callback)(std::string)) {
   
 }
