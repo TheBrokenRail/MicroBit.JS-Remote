0066c9de1929d577ff5dbb520abdb7e3f8b1db3f
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-11 07:27:57 -0400

    Update parser.h

--- Source/parser.h
+++ Source/parser.h
@@ -1,1 +1,11 @@
+class Command {
+  public:
+    std::string action;
+    std::vector<std::string> args;
+    Command(std::string newAction, std::vector<std::string> newArgs) {
+      action = newAction;
+      args = newArgs;
+    };
+};
+
 Command parseCommand(std::string command);
