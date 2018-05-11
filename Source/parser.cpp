11e87cd451a7c6faee1cac5b51b48ecb615d6995
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-11 07:27:42 -0400

    Update parser.cpp

--- Source/parser.cpp
+++ Source/parser.cpp
@@ -1,16 +1,6 @@
 #include <string>
 #include <vector>
 
-class Command {
-  public:
-    std::string action;
-    std::vector<std::string> args;
-    Command(std::string newAction, std::vector<std::string> newArgs) {
-      action = newAction;
-      args = newArgs;
-    };
-};
-
 Command parseCommand(std::string command) {
   std::string action = "";
   int stage = 0;
